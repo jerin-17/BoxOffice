c92a8c577329ea3f068ee122c300d59e6b541a75
jerin <jerinmanjadiyil@gmail.com>
2022-01-10 10:34:45 +0530

    added show page and its components

---
+++ src/pages/Show.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useReducer } from 'react';
+import { useParams } from 'react-router-dom';
+import Seasons from '../components/Show/Seasons';
+import ShowMainData from '../components/Show/ShowMainData';
+import { apiGet } from '../Misc/config';
+import Details from '../components/Show/Details';
+import Cast from '../components/Show/Cast';
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
+
+const Show = () => {
+  const { id } = useParams();
+
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  useEffect(() => {
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
+
+  
+
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
+
+  return <div>
+      < ShowMainData 
+        image = {show.image}
+        name = {show.name}
+        rating = {show.rating}
+        summary = {show.summary}
+        tags = {show.genres}
+      />
+          <div>
+              <h2>Details</h2>
+
+                <Details 
+                status = { show.status }
+                network = { show.network }
+                premiered = { show.premiered }
+                />
+          </div>
+          <div>
+              <h2>Seasons</h2>
+
+                <Seasons
+                seasons = { show._embedded.seasons }
+                />
+          </div>
+          <div>
+              <h2>Casts</h2>
+
+                <Cast  cast = { show._embedded.cast }/>
+          </div>
+      
+  </div>;
+};
+
+export default Show;
