24070cbd0c28224d3b4342ebd29a672a3f947134
jerin <jerinmanjadiyil@gmail.com>
2022-01-10 19:33:19 +0530

    added star functionality

--- src/components/Show/ShowGrid.js
+++ src/components/Show/ShowGrid.js
@@ -1,19 +1,41 @@
-import React from 'react'
-import IMAGE_NOT_FOUND from '../../image/not-found.png'
-import ShowCard from './ShowCard'
-import {FlexGrid} from '../styled'
-const ShowGrid = ({data}) => {
-    return (
-        <FlexGrid>
-            {
-                data.map(({show})=> <ShowCard key={show.id}  
-                id={show.id} name={show.name}
-                 image={show.image? show.image.medium : IMAGE_NOT_FOUND} 
-                 summary={show.summary} 
-                 />)
-            }
-        </FlexGrid>
-    )
-}
+import React from 'react';
+import ShowCard from './ShowCard';
 
-export default ShowGrid
+import { FlexGrid } from '../styled';
+
+import IMAGE_NOT_FOUND from '../../image/not-found.png';
+import { useShows } from '../../Misc/custom-hooks';
+
+const ShowGrid = ({ data }) => {
+  const [starredShows, dispatchStarred] = useShows();
+
+  return (
+    <FlexGrid>
+      {data.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
+
+        const onStarClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showId: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showId: show.id });
+          }
+        };
+
+        return (
+          <ShowCard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+            summary={show.summary}
+            onStarClick={onStarClick}
+            isStarred={isStarred}
+          />
+        );
+      })}
+    </FlexGrid>
+  );
+};
+
+export default ShowGrid;
