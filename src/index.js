1e5a0198ccaccf77982eb42d3ade92135b053da1
jerin <jerinmanjadiyil@gmail.com>
2022-01-11 00:19:20 +0530

    deployed the app on github

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 //import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render( <BrowserRouter>
+ReactDOM.render( <HashRouter>
 <App />
-</BrowserRouter>  , document.getElementById('root')
+</HashRouter>  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
